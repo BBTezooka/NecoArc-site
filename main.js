6a9675a2d2593eca0abaa0eefc740b3665fd0af3
NecoArtemchik <artem2004bazhenov@gmail.com>
2023-01-15 13:09:33 +0200

    Adaptiveshit go brrr

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ let cat = document.querySelector('.cat'),
  btn = document.querySelector('.blya');
 
 function myAnimation(){
-   let pos = 400;
-   let id = setInterval(frame, 10);
-   function frame(){
+      let pos = 400;
+      let id = setInterval(frame, 10);
+      function frame(){
       if (pos == 60){
           clearInterval(id);
         } else{
           pos--;
           cat.style.top = pos + 'px';
       }
+    }
   }
-}
 
 document.addEventListener("DOMContentLoaded",myAnimation());
 
